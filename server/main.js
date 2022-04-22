cbe2c350c2a0434da098aca26b61d5fb5b8d5f0e
Ev3rdown <35498857+Ev3rdown@users.noreply.github.com>
2022-04-22 15:44:06 +0000

    Add like in DB (no realtime update yet)

--- server/main.js
+++ server/main.js
@@ -1,43 +1,52 @@
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
+import { Mongo } from 'meteor/mongo'
 import { localDatas } from './local-datas.js'
 import { ClientRequest, ServerResponse } from 'http';
+import SimpleSchema from 'simpl-schema';
 import url from 'url';
 let keysJson = require('.keys.json');
 const MovieDB = require('node-themoviedb');
 
 const mdb = new MovieDB(keysJson.tmdb);
 
+var moviesLikes = new Mongo.Collection('moviesLikes');
+
+
+moviesLikes.schema = new SimpleSchema({
+  movieId: {type: SimpleSchema.Integer, required: true},
+  likes: {type: SimpleSchema.Integer, defaultValue: 0}
+});
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
-WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
-  res.writeHead(200);
-  res.end(JSON.stringify(localDatas));
-});
-
-/* WebApp.connectHandlers.use('/api/like/movie', (req, res, next) => {
-  console.log(req.url);
-  let queryParams = url.parse(req.url,false).query;
-  // On retire les query params de l'url
-  let urlWithoutQueryParams = req.url.split('?')[0];
-  let id = urlWithoutQueryParams.split("/")[1];
-  console.log((id.length>0?id:"errored id"));
-  res.writeHead(200);
-  res.end("Liked movie "+ (id.length>0?id:"errored id"));
-}); */
+async function getMovies() {
+  let movies = await mdb.discover.movie();
+  return movies;
+}
 
 Router.route('/api/movies/discover', async function () {
+  //let moLi = moviesLikes;
   /** @type {ClientRequest} */
   var req = this.request;
   /** @type {ServerResponse} */
   var res = this.response;
 
-  const movie = await mdb.discover.movie();
+  const movies = await mdb.discover.movie();
+  console.log(movies);
+  movies.data.results.forEach(movie => {
+    var mo = moviesLikes.findOne({ movieId: movie.id });
+    console.log(mo);
+    if (mo) {
+      movie.likes=mo.likes;
+    } else {
+      movie.likes=0;
+    }
+  });
   res.writeHead(200);
-  res.end(JSON.stringify(movie.data));
+  res.end(JSON.stringify(movies.data));
 
 }, {where: 'server'});
 
@@ -48,9 +57,12 @@ Router.route('/api/movies/like/:_id', async function () {
   var res = this.response;
 
   var params = this.params; // { _id: "5" }
-  var id = params._id;
+  var id = parseInt(params._id);
 
+  var up = moviesLikes.update({ movieId: id }, {$inc: { likes: 1 }},{ upsert: true });
+  var mo = moviesLikes.findOne({ movieId: id });
+  console.log(mo);
   res.writeHead(200);
-  res.end(JSON.stringify({"liked":id}));
+  res.end(JSON.stringify(mo));
 
 }, {where: 'server'});
