a00e1e8ecfc5af5f49e81407a2b61c45f00b1d6a
Ev3rdown <35498857+Ev3rdown@users.noreply.github.com>
2022-05-10 08:36:57 +0000

    Routing + navbar + detail page

--- client/main.js
+++ client/main.js
@@ -1,11 +1,37 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { fetch } from "meteor/fetch"
-Router.options.autoStart = false;
+//Router.options.autoStart = false;
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Router.configure({
+  layoutTemplate: 'main'
+});
+
+Router.route('/', {
+  template: 'home'
+});
+Router.route('/about');
+
+Router.route('/movie/:_id', function () {
+  var params = this.params; // { _id: "5" }
+  var id = params._id; // "5"
+  fetch('/api/movies/details/'+id).then(res=>{
+    res.json().then(json=>{
+      console.log(json);
+      this.render('movie', {
+        data: function () {
+          console.log(json.title);
+          console.log("test");
+          return json;
+        }
+      });
+    })
+  });
+});
+
+/* Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -21,7 +47,7 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
-});
+}); */
 
 Template.home.onCreated(function homeOnCreated() {
   let ctrl = this;
@@ -43,6 +69,11 @@ Template.home.events({
     fetch('/api/movies/like/'+movieId).then(res=>{
       res.json().then(json=>{
         console.log(json);
+        if (json.success == true){
+          var nblikes = (parseInt(document.getElementsByClassName('likes-'+movieId)[0].dataset.nblikes) + 1).toString();
+          document.getElementsByClassName('likes-'+movieId)[0].dataset.nblikes = nblikes;
+          document.getElementsByClassName('likes-675353')[0].getElementsByClassName('likes')[0].innerText = nblikes;
+        }
       })
     });
   },
--- server/main.js
+++ server/main.js
@@ -11,21 +11,17 @@ const MovieDB = require('node-themoviedb');
 const mdb = new MovieDB(keysJson.tmdb);
 
 var moviesLikes = new Mongo.Collection('moviesLikes');
+var moviesComments = new Mongo.Collection('moviesComments');
 
 
-moviesLikes.schema = new SimpleSchema({
+/* moviesLikes.schema = new SimpleSchema({
   movieId: {type: SimpleSchema.Integer, required: true},
   likes: {type: SimpleSchema.Integer, defaultValue: 0}
-});
+}); */
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
-
-async function getMovies() {
-  let movies = await mdb.discover.movie();
-  return movies;
-}
 
 Router.route('/api/movies/discover', async function () {
   //let moLi = moviesLikes;
@@ -35,6 +31,7 @@ Router.route('/api/movies/discover', async function () {
   var res = this.response;
 
   const movies = await mdb.discover.movie();
+  console.log(movies.data.results);
   movies.data.results.forEach(movie => {
     var mo = moviesLikes.findOne({ movieId: movie.id });
     movie.likes=0;
@@ -57,3 +54,44 @@ Router.route('/api/movies/like/:_id', function () {
   res.writeHead(200);
   res.end(JSON.stringify({success: (up==1?true:false)}));
 }, {where: 'server'});
+
+Router.route('/api/movies/comment/:_id', function () {
+  /** @type {ClientRequest} */
+  var req = this.request;
+  /** @type {ServerResponse} */
+  var res = this.response;
+
+  var params = this.params;
+  var id = parseInt(params._id);
+
+  var commented = moviesComment.insert({movieId: id, content: comment, createdAt: new Date() });
+  res.writeHead(200);
+  res.end(JSON.stringify({success: (commented==1?true:false)}));
+}, {where: 'server'});
+
+
+Router.route('/api/movies/details/:_id', async function () {
+  /** @type {ClientRequest} */
+  var req = this.request;
+  /** @type {ServerResponse} */
+  var res = this.response;
+
+  var params = this.params;
+  var id = parseInt(params._id);
+
+  const movie = await mdb.movie.getDetails({
+    pathParameters: {
+      movie_id: id,
+    },
+  });
+
+  res.writeHead(200);
+  res.end(JSON.stringify(movie.data));
+}, {where: 'server'});
+
+//https://github.com/longshotlabs/simpl-schema#required
+//http://iron-meteor.github.io/iron-router/#global-default-options
+//https://developers.themoviedb.org/3/discover/movie-discover
+//https://www.npmjs.com/package/node-themoviedb#common-usage-of-methods
+//https://guide.meteor.com/collections.html#schemas
+//file:///D:/EPSI/Cours%20Services%20Web/web-services-v2.pdf
